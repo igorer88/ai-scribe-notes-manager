aaaedde2615b477849238ffdbd2cc1ec48509b3e
Igor Escalona <igorer88@users.noreply.github.com>
2025-10-26 00:15:24 -0400

    feat: Add logging to database seeding process

--- src/database/run-seeds.ts
+++ src/database/run-seeds.ts
@@ -1,12 +1,18 @@
+import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { SeederModule } from './seeder.module'
 import { DatabaseSeeder } from './seeds/DatabaseSeeder'
 
 async function bootstrap(): Promise<void> {
+  const logger = new Logger('Bootstrap')
+
   const app = await NestFactory.createApplicationContext(SeederModule)
   const seeder = app.get(DatabaseSeeder)
+
+  logger.log('Starting database seeding...')
   await seeder.run()
+  logger.log('Database seeding completed.')
   await app.close()
 }
 
